82624792fd4f4d6393da649f97bb0712c912b500
semhar-z <semharzewoldi@gmail.com>
2024-11-22 14:10:16 -0500

    third commit

--- src/Services/sw-api.js
+++ src/Services/sw-api.js
@@ -4,5 +4,5 @@ export async function getAllStarships() {
   const response = await fetch(`${URL}/starships/`);
   if (!response.ok) throw new Error('Network response was not ok');
   const data = await response.json();
-  return data.results; // SWAPI returns starships in a `results` array
+  return data.results; 
 }
